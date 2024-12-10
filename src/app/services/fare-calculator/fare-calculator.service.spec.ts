e2579728cb62e57d88629af1a6b913d007c5e1d8
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-10 18:29:45 +0400

    chore(general): generate services

---
+++ src/app/services/fare-calculator/fare-calculator.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { FareCalculatorService } from './fare-calculator.service';
+
+describe('FareCalculatorService', () => {
+  let service: FareCalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FareCalculatorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
