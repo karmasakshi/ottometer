20c9320975428be866ca46ff39aa4d514dc605f6
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-10 19:33:41 +0400

    chore(general): add supabase

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { SupabaseService } from '../supabase/supabase.service';
+import { AuthChangeEvent, AuthSession, User } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private readonly _supabaseService = inject(SupabaseService);
 
-  constructor() { }
+  private readonly _user: WritableSignal<User | null>;
+
+  public constructor() {
+    this._user = signal(null);
+
+    this._supabaseService.supabaseClient.auth.onAuthStateChange(
+      (_authChangeEvent: AuthChangeEvent, authSession: AuthSession | null) => {
+        this._user.set(authSession?.user ?? null);
+      },
+    );
+  }
 }
--- src/app/services/supabase/supabase.service.ts
+++ src/app/services/supabase/supabase.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {
+  AuthChangeEvent,
+  AuthError,
+  AuthSession,
+  OAuthResponse,
+  SupabaseClient,
+  UserResponse,
+  createClient,
+} from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupabaseService {
+  private readonly _supabaseClient: SupabaseClient;
 
-  constructor() { }
+  constructor() { 
+    this._supabaseClient = createClient(
+      import.meta.env.NG_APP_SUPABASE_URL ?? '',
+      import.meta.env.NG_APP_SUPABASE_KEY ?? '',
+    );
+  }
+
+  public get supabaseClient(): SupabaseClient {
+    return this._supabaseClient;
+  }
 }
