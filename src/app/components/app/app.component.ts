acad7062360f669fbf1bff7c4e8809401bf1e142
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-10 17:40:04 +0400

    chore(general): add app component

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'jet-root',
+  selector: 'jet-app',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jet';
+
 }
