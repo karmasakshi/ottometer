3f7455fa29d1e9400a97e8902372ef393352cd63
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-11 22:06:38 +0400

    chore(general): clean-up

--- src/app/database/04_functions.sql
+++ src/app/database/04_functions.sql
@@ -34,6 +34,32 @@ $$ language plpgsql;
 
 --
 
+create or replace function insert_report_and_auto()
+returns trigger
+security definer
+set search_path = '' as $$
+declare
+    new_auto_id uuid;
+begin
+    select id into new_auto_id
+    from autos
+    where plate_state_code = new.plate_state_code
+      and plate_district_code = new.plate_district_code
+      and plate_series_code = new.plate_series_code
+      and plate_vehicle_number = new.plate_vehicle_number;
+    if new_auto_id is null then
+      insert into autos (plate_state_code, plate_district_code, plate_series_code, plate_vehicle_number)
+      values (new.plate_state_code, new.plate_district_code, new.plate_series_code, new.plate_vehicle_number)
+      returning id into new_auto_id;
+    end if;
+    insert into reports (auto_id, reporter_id, area_from, area_to, fare_difference_in_inr, meter_distance_in_km, meter_fare_in_inr, meter_waiting_time_in_min, is_tamper_indicator_on, time_in, time_out, type)
+    values (new_auto_id, new.reporter_id, new.area_from, new.area_to, new.fare_difference_in_inr, new.meter_distance_in_km, new.meter_fare_in_inr, new.meter_waiting_time_in_min, new.is_tamper_indicator_on, new.time_in, new.time_out, new.type);
+    return new;
+end;
+$$ language plpgsql;
+
+--
+
 create or replace function update_updated_at()
 returns trigger
 security definer
--- src/app/database/05_triggers.sql
+++ src/app/database/05_triggers.sql
@@ -9,21 +9,29 @@ execute function insert_profile();
 
 create trigger update_updated_at
 before update on public.profiles
-for each row execute function update_updated_at();
+for each row
+execute function update_updated_at();
 
 -- public.autos
 
 create trigger update_updated_at
 before update on public.autos
-for each row execute function update_updated_at();
+for each row
+execute function update_updated_at();
 
 -- public.reports
 
-create trigger update_updated_at
-before update on public.reports
-for each row execute function update_updated_at();
-
 create trigger check_daily_report_limit
-before insert on reports
+before insert on public.reports
 for each row
 execute function check_daily_report_limit();
+
+create trigger insert_report_and_auto
+before insert on public.reports
+for each row
+execute function insert_report_and_auto();
+
+create trigger update_updated_at
+before update on public.reports
+for each row
+execute function update_updated_at();
