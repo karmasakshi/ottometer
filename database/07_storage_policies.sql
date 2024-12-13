2eb51f4a1fa848ec07fcfafc61e5acc1fc306f23
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-14 00:50:32 +0400

    chore(general): clean-up

--- database/07_storage_policies.sql
+++ database/07_storage_policies.sql
@@ -1,3 +1,15 @@
-create policy "give users access to own folder 1oj01fe_0" on storage.objects for select to public using (bucket_id = 'avatars' and (select auth.uid()::text) = (storage.foldername(name))[1]);
+create policy "allow users to view their own folder"
+on storage.objects
+for select
+using (
+    bucket_id = 'avatars'
+    and auth.uid()::text = (storage.foldername(name))[1]
+);
 
-create policy "give users access to own folder 1oj01fe_1" on storage.objects for insert to public with check (bucket_id = 'avatars' and (select auth.uid()::text) = (storage.foldername(name))[1]);
+create policy "allow users to insert into their own folder"
+on storage.objects
+for insert
+with check (
+    bucket_id = 'avatars'
+    and auth.uid()::text = (storage.foldername(name))[1]
+);
